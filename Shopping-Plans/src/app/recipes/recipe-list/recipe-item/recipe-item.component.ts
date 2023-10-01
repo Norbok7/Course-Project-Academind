03f4df5c5848858fff110ccf35243ab9e78ad248
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-10-01 16:21:26 -0500

    shopping Plans code along
    adding base website layout buttons etc

---
+++ Shopping-Plans/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit{
+recipes: Recipe [] = [
+  new Recipe('A test recipe', 'simply a test', 'https://www.simplyrecipes.com/thmb/KRw_r32s4gQeOX-d07NWY1OlOFk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1c-c2b1885d27d4481c9cfe6f6286a64342.jpg')
+];
+
+constructor() {
+
+}
+
+ngOnInit() {
+
+}
+}
---
+++ Shopping-Plans/src/app/shopping-list/shopping-edits/shopping-edits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edits',
+  templateUrl: './shopping-edits.component.html',
+  styleUrls: ['./shopping-edits.component.css']
+})
+export class ShoppingEditsComponent {
+
+}
